68e8e26f28db7c18ce127eec9c642848a941bcd5
Moses Ramirez <mosesgramirez@gmail.com>
2021-03-08 23:48:21 -0600

    Experimenting with middleware. Add responseTime.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const responseTime = require('response-time')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -16,6 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(responseTime());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
