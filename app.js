5ee0bfb0155fb1b6f01d109434ecd24f16bc556b
Moses Ramirez <mosesgramirez@gmail.com>
2021-03-18 15:04:49 -0500

    Update cookie to Express Session

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser'); express-session implements cookies
 const logger = require('morgan');
-const responseTime = require('response-time')
+const responseTime = require('response-time');
+const session = require('express-session');
+const fileStore = require('session-file-store')(session);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -34,10 +36,19 @@ app.use(responseTime());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false, // example settings
+  store: new fileStore()
+}));
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session)
+  if (!req.session.user) {
     // console.log('This is req.headers: ', req.headers);
     const authHeader = req.headers.authorization;
     // console.log('This is authHeader: ',authHeader);
@@ -60,7 +71,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === 'admin' && pass === 'password') {
-      res.cookie('user', 'admin', {signed: true}); // Express Response API
+      req.session.user = 'admin' // 
       return next(); // authorization successful
     } else {
         const err = new Error('You are not authenticated!');
@@ -69,7 +80,7 @@ function auth(req, res, next) {
         return next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
       return next();
     } else {
       const err = new Error('You are not authenticated!');
