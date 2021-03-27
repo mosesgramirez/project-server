36b5fce72ababce1a6ddbc65c868742014f84c91
Moses Ramirez <mosesgramirez@gmail.com>
2021-03-27 08:59:10 -0500

    Set up OAuth2 integration with Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '471722054045633',
+        clientSecret: '398f5f34c8961038c129ad36e8145866'
+    }
 }
